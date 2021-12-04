fabd0441223505f91d3e30afd3403973371244c8
Naveen2706 <naveen271996@gmail.com>
2021-12-04 09:45:40 +0530

    enum example

---
+++ JavaRevision/src/Tastes.java
@@ -0,0 +1,28 @@
+public enum Tastes {                                                //enum outside class
+    SWEET,
+    SPICY,
+    BITTER,
+    SAULTY
+}
+
+class Main3 {
+    enum WorkShifts {                                              //enum inside a class
+        MORNING,
+        AFTERNOON,
+        NIGHT
+    }
+
+    public static void main(String[] args) {
+        Tastes objTastes = Tastes.SWEET;                           //initialising enum 1
+        System.out.println("enum 1 :" + objTastes);
+        WorkShifts objWork = WorkShifts.MORNING;                   //initialising enum 2
+        System.out.println("enum 2 :" + objWork);
+
+        switch (objTastes) {                                        // enum use in switch statement
+            case SPICY -> System.out.println("the taste is spicy");
+            case SWEET -> System.out.println("the taste is sweet");
+            case SAULTY -> System.out.println("the taste is saulty");
+            case BITTER -> System.out.println("the taste is bitter");
+        }
+    }
+}
