bf96d77fbfa9b8e55de6a5902efce76c2e540803
Naveen2706 <naveen271996@gmail.com>
2021-12-16 12:25:07 +0530

    functional interface implementation by anonymous class and class instance

--- JavaRevision/src/functional_Interface/FunctionalInterfaceExample.java
+++ JavaRevision/src/functional_Interface/FunctionalInterfaceExample.java
@@ -5,22 +5,29 @@ import java.util.Scanner;
 @FunctionalInterface
 interface Sam {
     void name(String name);                     //only one abstract method in functional interface
-                                                //It can have any number of default, static methods
+    //It can have any number of default, static methods
 }
 
 public class FunctionalInterfaceExample {
 
     void name(String name) {
-        System.out.println(name);
+        System.out.println("name by class object :" + name);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        FunctionalInterfaceExample obj = new FunctionalInterfaceExample();
+        FunctionalInterfaceExample obj = new FunctionalInterfaceExample();      //implementation by class obj
 
+        Sam interfaceObj = new Sam() {                            //implementation by anonymous class
+            @Override
+            public void name(String name) {
+                System.out.println("name by anonymous class :" + name);
+            }
+        };
         System.out.println("Enter the name : ");
         String name = scanner.next();
-        obj.name(name);
+        obj.name(name);                 //implementation by class obj
+        interfaceObj.name(name);        //implementation by anonymous class
         scanner.close();
     }
 }
